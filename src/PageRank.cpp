4def7116b84749dff9ab7ee8f0a60718f4f549d4
Sebastian Garces <sgarces@PCOF-60744-PROYECTO-50-CLAUDIA-ZEA-BL15-P2-ACA-2.local>
2016-11-28 14:06:50 -0500

    Corrección Sustentación MPI

--- src/PageRank.cpp
+++ src/PageRank.cpp
@@ -13,6 +13,29 @@ static double TOLERANCE = 0.000000000000000000000001;
 
 #define k 2
 
+class Matrix {
+	int x, y;
+	double **matrix;
+public:
+	Matrix (int, int);
+	void setValue (int x, int y, double value){
+		matrix[x][y] = value;
+	}
+	double getValue (int x, int y){
+		return matrix[x][y];
+	}
+	double * getMatrix (){
+		return *matrix;
+	} 
+};
+
+Matrix::Matrix (int x, int y){
+	matrix = new double*[x];
+	for (int i = 0; i < x; i++){
+		matrix[i] = new double[y];
+	}
+}
+
 double maximumValue(double array[], int n)
 {
 	double max = array[0];
@@ -48,12 +71,11 @@ int main(int argc, char **argv) {
 	int p, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	char mpi;
 
 	int n = abs(atoi(argv[1]));
 	double d = abs(atof(argv[2]));
 
-	double matrix[n][n];
+	Matrix matrix (n,n);
 	double sumarColumnas [n];
 	fill(sumarColumnas, sumarColumnas + n, 0);
 	double vector[n];
@@ -68,13 +90,13 @@ int main(int argc, char **argv) {
 
 	const int BLOCKROWS = n/k;      /* Numero de filas a descomponer */
 	const int BLOCKCOLS = n;        /* Numero de columnas a descomponer */
-	double subMatrizMpi[BLOCKCOLS][BLOCKROWS];
+	Matrix subMatrizMpi(BLOCKCOLS,BLOCKROWS);
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			matrix[i][j] = 0;
+			matrix.setValue(i,j,0);
 		}
-		subMatrizMpi[0][i] = 0;
+		subMatrizMpi.setValue(0,i,0);
 	}
 
 	MPI_Datatype blocktype;
@@ -84,88 +106,161 @@ int main(int argc, char **argv) {
 	MPI_Type_create_resized(blocktype2, 0, sizeof(double), &blocktype);
 	MPI_Type_commit(&blocktype);
 
-	srand(static_cast<unsigned>(time(0)));
+	unsigned seed = static_cast<unsigned>(time(0));
+	srand(seed);
 
-	for (int j = 0; j < n; j++) {
-		int i = 1 + (static_cast <int> (rand()) % n) - 1;
-		if (i != j) {
-			matrix[i][j] = static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
-			cntNum--;
-		}else {
-			j--;
+	if (rank == 0){
+		MPI_Send(&seed, 1, MPI_UNSIGNED, 1, 0, MPI_COMM_WORLD);
+
+		for (int j = 0; j < n; j++) {
+			int i = 1 + (static_cast <int> (rand()) % n) - 1;
+			if (i != j) {
+				matrix.setValue(i,j,static_cast <double> (rand()) / static_cast <double> (RAND_MAX));
+				cntNum--;
+			}else {
+				j--;
+			}
 		}
-	}
 
-	for(int i = 0; i < cntNum; i++) {
-		int ri = 1 + (static_cast <int> (rand()) % n) - 1;
-		int rj = 1 + (static_cast <int> (rand()) % n) - 1;
-		if (ri == rj || matrix[ri][rj] != 0) {
-			i--;
-		} else {
-			matrix[ri][rj] = static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+		for(int i = 0; i < cntNum; i++) {
+			int ri = 1 + (static_cast <int> (rand()) % n) - 1;
+			int rj = 1 + (static_cast <int> (rand()) % n) - 1;
+			if (ri == rj || matrix.getValue(ri,rj) != 0) {
+				i--;
+			} else {
+				matrix.setValue(ri,rj,static_cast <double> (rand()) / static_cast <double> (RAND_MAX));
+			}
 		}
-	}
 
-	//Sumo las columnas, la suma se divide por 1 y se agrega esto a un arreglo
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			sumarColumnas[i] = sumarColumnas[i] + matrix[j][i];
+		//Sumo las columnas, la suma se divide por 1 y se agrega esto a un arreglo
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				sumarColumnas[i] = sumarColumnas[i] + matrix.getValue(j,i);
+			}
+			sumarColumnas[i] = 1/sumarColumnas[i];
 		}
-		sumarColumnas[i] = 1/sumarColumnas[i];
-	}
 
-	//Cada posicion de cada columna se multiplica por el numero correspondiente calculado anteriormente
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			matrix[j][i] = sumarColumnas[i] * matrix[j][i];
+		//Cada posicion de cada columna se multiplica por el numero correspondiente calculado anteriormente
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				matrix.setValue(j,i,sumarColumnas[i] * matrix.getValue(j,i));
+			}
 		}
+
+		for(int i = 0; i < n; i++) {
+			vector[i] = (double)1 / n;
+		}
+
+	} else {
+		MPI_Recv(&seed, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		for (int j = 0; j < n; j++) {
+			int i = 1 + (static_cast <int> (rand()) % n) - 1;
+			if (i != j) {
+				matrix.setValue(i,j,static_cast <double> (rand()) / static_cast <double> (RAND_MAX));
+				cntNum--;
+			}else {
+				j--;
+			}
+		}
+
+		for(int i = 0; i < cntNum; i++) {
+			int ri = 1 + (static_cast <int> (rand()) % n) - 1;
+			int rj = 1 + (static_cast <int> (rand()) % n) - 1;
+			if (ri == rj || matrix.getValue(ri,rj) != 0) {
+				i--;
+			} else {
+				matrix.setValue(ri,rj,static_cast <double> (rand()) / static_cast <double> (RAND_MAX));
+			}
+		}
+
+		//Sumo las columnas, la suma se divide por 1 y se agrega esto a un arreglo
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				sumarColumnas[i] = sumarColumnas[i] + matrix.getValue(j,i);
+			}
+			sumarColumnas[i] = 1/sumarColumnas[i];
+		}
+
+		//Cada posicion de cada columna se multiplica por el numero correspondiente calculado anteriormente
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				matrix.setValue(j,i,sumarColumnas[i] * matrix.getValue(j,i));
+			}
+		}
+
+		for(int i = 0; i < n; i++) {
+			vector[i] = (double)1 / n;
+		}
+
 	}
 
-	for(int i = 0; i < n; i++) {
-		vector[i] = (double)1 / n;
-	}
+	
 
 	double maxValue = 1;
 	int count = 0;
 
 	int disps[k];
 	int counts[k];
-	int raizK = ceil(sqrt(k));
+	//int raizK = ceil(sqrt(k));
 
-	for (int ii=0; ii < raizK; ii++) {
-		for (int jj=0; jj < raizK; jj++) {
-			disps[ii * raizK + jj] = matrix[ii][jj];
-			counts[ii * raizK + jj] = 1;
-		}
+	for (int ii=1; ii < k; ii++) {
+		disps[ii] = matrix.getValue(BLOCKROWS * k,0);
+		counts[ii] = 1;
 	}
 
 	while(true) {
 
-		MPI_Scatterv(matrix, counts, disps, blocktype, subMatrizMpi, BLOCKROWS * BLOCKCOLS, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		if (rank == 0){
+			MPI_Scatterv(matrix.getMatrix(), counts, disps, blocktype, subMatrizMpi.getMatrix(), BLOCKROWS * BLOCKCOLS, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-
-		if (maxValue > TOLERANCE) {
-			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < n; j++) {
-					resultVector[i] += (matrix[i][j] * vector[j]);
+			if (maxValue > TOLERANCE) {
+				for(int i = 0; i < BLOCKROWS; i++) {
+					for(int j = 0; j < BLOCKCOLS; j++) {
+						resultVector[i] += (matrix.getValue(i,j) * vector[j]);
+					}
 				}
+			} else {
+				break;
 			}
+
+			for (int i = 0; i < n; i++) {
+				vectorParada[i] = abs(resultVector[i] - vector[i]);
+			}
+
+			maxValue = maximumValue(vectorParada, n);
+
+			for (int i = 0; i < n; i++) {
+				vector[i] = resultVector[i];
+			}
+
+			fill(resultVector, resultVector + n, 0);
+
 		} else {
-			break;
+
+			if (maxValue > TOLERANCE) {
+				for(int i = rank * BLOCKROWS; i < (rank * BLOCKROWS) + (BLOCKROWS - 1); i++) {
+					for(int j = 0; j < BLOCKCOLS; j++) {
+						resultVector[rank * BLOCKROWS] += (matrix.getValue(i,j) * vector[j]);
+					}
+				}
+			} else {
+				break;
+			}
+
+			for (int i = 0; i < n; i++) {
+				vectorParada[i] = abs(resultVector[i] - vector[i]);
+			}
+
+			maxValue = maximumValue(vectorParada, n);
+
+			for (int i = 0; i < n; i++) {
+				vector[i] = resultVector[i];
+			}
+
+			fill(resultVector, resultVector + n, 0);
+
 		}
-
-		for (int i = 0; i < n; i++) {
-			vectorParada[i] = abs(resultVector[i] - vector[i]);
-		}
-
-		maxValue = maximumValue(vectorParada, n);
-
-		for (int i = 0; i < n; i++) {
-			vector[i] = resultVector[i];
-		}
-
-		fill(resultVector, resultVector + n, 0);
-
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
